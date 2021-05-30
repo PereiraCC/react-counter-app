f7360a37029c659744ed1ca2ff6a6acb68c4cd02
PerairaCC <carloscoto0103@gmail.com>
2021-05-30 09:47:30 -0600

    Mostrar variables en el HTML

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,10 +1,17 @@
+// import React, { Fragment } from 'react'
 import React from 'react'
 
+
 const PrimeraApp = () => {
+
+    const saludo = 'Hola Mundo';
+
     return (
-        <h1>
-            Hola Mundo
-        </h1>
+        <>
+            <h1>{ saludo }</h1>
+            {/* <pre>{ JSON.stringify(saludo, null, 3) }</pre> */}
+            <p>Mi primera aplicacion</p>
+        </>
     );
 }
 
