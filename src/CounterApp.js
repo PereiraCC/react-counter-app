820b73630ea8338bacb7496d36e4f40c7facbfbc
PerairaCC <carloscoto0103@gmail.com>
2021-05-30 12:05:54 -0600

    Creacion de las funciones handleSubtract y handleReset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types'
 
 const CounterApp = ( {value} ) => {
 
-    const [ counter, setCounter ] = useState(0); // []
+    const [ counter, setCounter ] = useState(value); // []
 
     // handleAdd
-    const handleAdd = () => {
-        setCounter(counter + 1);
-        // setCounter((c) => c + 1);
+    const handleAdd = () => setCounter(counter + 1);
 
-    }
+    const handleSubtract = () => setCounter(counter - 1);
+    
+    const handleReset = () =>  setCounter(value);
+
 
     return (
         <>
@@ -18,6 +19,8 @@ const CounterApp = ( {value} ) => {
             <h2> { counter } </h2>
 
             <button onClick= { handleAdd }>+1</button>
+            <button onClick= { handleReset }>Reset</button>
+            <button onClick= { handleSubtract }>-1</button>
         </>
     )
 }
